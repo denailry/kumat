79e5de88ecb37155c10e11cae270a87a073aad89
denailry <daniel93ryan@gmail.com>
2017-07-30 02:34:25 +0700

    fix: Non-activate floating action button

--- app/src/main/java/com/example/user/kumat/PengaturanActivity.java
+++ app/src/main/java/com/example/user/kumat/PengaturanActivity.java
@@ -20,14 +20,14 @@ public class PengaturanActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Pengaturan");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
 
