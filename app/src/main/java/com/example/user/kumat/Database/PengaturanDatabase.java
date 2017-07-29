bd90d0b1b5d04b0a6c98bbabaede8706a36c833d
denailry <daniel93ryan@gmail.com>
2017-07-30 02:59:02 +0700

    new: PengaturanDatabase 	Settings data has been handled by PengaturanActivity 	and PengaturanDatabase

---
+++ app/src/main/java/com/example/user/kumat/Database/PengaturanDatabase.java
@@ -0,0 +1,71 @@
+package com.example.user.kumat.Database;
+
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+/**
+ * Created by denail on 17/07/30.
+ */
+
+@Table( database = MyDatabase.class)
+public class PengaturanDatabase extends BaseModel {
+
+    @PrimaryKey
+    @Column
+    private int id;
+
+    @Column
+    private boolean isNotifHomeAllowed;
+    @Column
+    private boolean isNotifPOAllowed;
+    @Column
+    private boolean isSyncAllowed;
+
+    public PengaturanDatabase() {
+        this.id = 1;
+        this.isNotifHomeAllowed = true;
+        this.isNotifPOAllowed = true;
+        this.isSyncAllowed = true;
+    }
+
+    public PengaturanDatabase(boolean isNotifHomeAllowed, boolean isNotifPOAllowed, boolean isSyncAllowed) {
+        this.id = 1;
+        this.isNotifHomeAllowed = isNotifHomeAllowed;
+        this.isNotifPOAllowed = isNotifPOAllowed;
+        this.isSyncAllowed = isSyncAllowed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isNotifHomeAllowed() {
+        return isNotifHomeAllowed;
+    }
+
+    public void setNotifHomeAllowed(boolean notifHomeAllowed) {
+        isNotifHomeAllowed = notifHomeAllowed;
+    }
+
+    public boolean isNotifPOAllowed() {
+        return isNotifPOAllowed;
+    }
+
+    public void setNotifPOAllowed(boolean notifPOAllowed) {
+        isNotifPOAllowed = notifPOAllowed;
+    }
+
+    public boolean isSyncAllowed() {
+        return isSyncAllowed;
+    }
+
+    public void setSyncAllowed(boolean syncAllowed) {
+        isSyncAllowed = syncAllowed;
+    }
+}
--- app/src/main/java/com/example/user/kumat/PengaturanActivity.java
+++ app/src/main/java/com/example/user/kumat/PengaturanActivity.java
@@ -6,8 +6,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+
+import com.example.user.kumat.Database.PengaturanDatabase;
+import com.raizlabs.android.dbflow.sql.language.Select;
 
 public class PengaturanActivity extends AppCompatActivity {
 
@@ -19,6 +24,21 @@ public class PengaturanActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Pengaturan");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        PengaturanDatabase setting = new Select()
+                .from(PengaturanDatabase.class)
+                .querySingle();
+        if(setting == null) {
+            setting = new PengaturanDatabase(true, true, true);
+            setting.save();
+        }
+
+        Log.d("TEST-1", String.valueOf(
+                setting.isNotifHomeAllowed() + " " + setting.isNotifPOAllowed() + " " + setting.isSyncAllowed()
+        ));
+        ((CheckBox) findViewById(R.id.ck_notif_home)).setChecked(!setting.isNotifHomeAllowed());
+        ((CheckBox) findViewById(R.id.ck_notif_po)).setChecked(!setting.isNotifPOAllowed());
+        ((CheckBox) findViewById(R.id.ck_sync)).setChecked(!setting.isSyncAllowed());
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -39,4 +59,27 @@ public class PengaturanActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void onCheckBoxClicked(View view) {
+        boolean checked = ((CheckBox) view).isChecked();
+
+        PengaturanDatabase setting = new Select()
+                .from(PengaturanDatabase.class)
+                .querySingle();
+
+        switch(view.getId()) {
+            case R.id.ck_notif_home:
+                setting.setNotifHomeAllowed(!checked);
+                break;
+            case R.id.ck_notif_po:
+                setting.setNotifPOAllowed(!checked);
+                break;
+            case R.id.ck_sync:
+                setting.setSyncAllowed(!checked);
+                break;
+        }
+        setting.save();
+        Log.d("TEST-2", String.valueOf(
+                setting.isNotifHomeAllowed() + " " + setting.isNotifPOAllowed() + " " + setting.isSyncAllowed()
+        ));
+    }
 }
